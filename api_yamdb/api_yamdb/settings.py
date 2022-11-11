14e2a8723cedfe1ce20ff6fccf86fe2adacc1e8d
Михаил Усенко <miha55rambler.ru@mail.ru>
2022-11-11 18:54:02 +0300

    Test 3.6

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1ilz@4zqj=rq&agdol^##zgl9vs')
+SECRET_KEY = os.getenv('SECRET_KEY', default='30342')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
